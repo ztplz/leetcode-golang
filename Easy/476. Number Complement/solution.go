bffde23daf746ee72d3b3c782b92cb8cc9f6ac32
ztplz <mysticzt@gmail.com>
2018-02-23 00:25:27 +0800

    476.Number Complement

---
+++ Easy/476. Number Complement/solution.go
@@ -0,0 +1,18 @@
+/*
+ Author: ztplz
+ leetcode profile: https://leetcode.com/ztplz/
+ Email:     mysticzt@gmail.com
+ Url:     https://leetcode.com/problems/number-complement/description/
+ Runtime: 0ms
+*/import "strconv"
+
+func findComplement(num int) int {
+	s := strconv.FormatInt(int64(num), 2)
+	mask := 2
+
+	for i := 0; i < len(s)-1; i++ {
+		mask = mask * 2
+	}
+
+	return ^num & (mask - 1)
+}
