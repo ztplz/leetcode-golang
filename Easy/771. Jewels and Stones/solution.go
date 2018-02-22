6472a3ea37b4c035f4889c2b9417930d52a2d6ad
ztplz <mysticzt@gmail.com>
2018-02-22 23:37:01 +0800

    initial commit

---
+++ Easy/771. Jewels and Stones/solution.go
@@ -0,0 +1,22 @@
+/*
+ Author:     mysticzt@gmail.com
+ Url:     https://leetcode.com/problems/jewels-and-stones/description/
+ Runtime: 0ms
+*/
+
+func numJewelsInStones(J string, S string) int {
+	var count int
+	m := make(map[rune]int)
+
+	for _, v1 := range J {
+		m[v1]++
+	}
+
+	for _, v2 := range S {
+		if _, ok := m[v2]; ok {
+			count++
+		}
+	}
+
+	return count
+}
