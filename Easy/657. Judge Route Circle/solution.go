e1d4632f087e1aaeb8e874deb832dfb15dee375f
ztplz <mysticzt@gmail.com>
2018-02-22 23:48:01 +0800

    657.Judge Route Circle

---
+++ Easy/657. Judge Route Circle/solution.go
@@ -0,0 +1,9 @@
+/*
+ Author:     mysticzt@gmail.com
+ Url:     https://leetcode.com/problems/judge-route-circle/description/
+ Runtime: 4ms
+*/import "strings"
+
+func judgeCircle(moves string) bool {
+	return strings.Count(moves, "U") == strings.Count(moves, "D") && strings.Count(moves, "L") == strings.Count(moves, "R")
+}
