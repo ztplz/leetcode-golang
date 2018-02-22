dd2974571714c0dfee5c952d216f3fde1899616b
ztplz <mysticzt@gmail.com>
2018-02-23 00:15:11 +0800

    561.Array Partition I

---
+++ Easy/561. Array Partition I/solution.go
@@ -0,0 +1,20 @@
+/*
+ Author: ztplz
+ leetcode profile: https://leetcode.com/ztplz/
+ Email:     mysticzt@gmail.com
+ Url:     https://leetcode.com/problems/array-partition-i/description/
+ Runtime: 80ms
+*/import "sort"
+
+func arrayPairSum(nums []int) int {
+	sort.Ints(nums)
+
+	n := len(nums) / 2
+	sum := 0
+
+	for i := 0; i < n; i++ {
+		sum += nums[2*i]
+	}
+
+	return sum
+}
