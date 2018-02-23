10f9423d6273515715931a77d1988bc2d6dec718
ztplz <mysticzt@gmail.com>
2018-02-23 21:59:22 +0800

    215. Kth Largest Element in an Array

---
+++ Medium/215. Kth Largest Element in an Array/solution.go
@@ -0,0 +1,13 @@
+/*
+ Author: ztplz
+ leetcode profile: https://leetcode.com/ztplz/
+ Email:     mysticzt@gmail.com
+ Url:     https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+ Runtime: 8ms
+*/import "sort"
+
+func findKthLargest(nums []int, k int) int {
+	sort.Ints(nums)
+
+	return nums[len(nums)-k]
+}
